2cb0eabb68312ae20d2a1b21f62b3d888420fe59
Soren Goyal <sorengoyal@Sorens-MBP.hitronhub.home>
2023-04-23 10:40:01 -0700

    Updated lambda code to read from DynamoDB
        Added a new role to lambda to read from dynamodb
        Ref: https://github.com/widdix/aws-cf-templates/blob/907515a62195730894d0c40f517c1b844f5e4f42/security/config.yaml#L184
    
        Added calls to dyanmodb
        Ref: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/client/put_item.html

--- function/get.py
+++ function/get.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+from typing import Dict
+
 import boto3
 import json
 
 print('Loading function')
 dynamo = boto3.client('dynamodb')
+class AtmModel:
+    def __init__(self, id: int, address: str, provider: str, rating: float, created_on: datetime):
+        self.id = id
+        self.address = address
+        self.provider = provider
+        self.rating = rating
+        self.created_on = created_on
 
+    def __init__(self, json_model: Dict):
+        self.id = json_model["id"]
+        self.address = json_model["address"]
+        self.provider = json_model["provider"]
+        self.rating = json_model["rating"]
+        self.created_on = json_model["created_on"]
 
 def respond(err, res=None):
     return {
@@ -15,12 +31,18 @@ def respond(err, res=None):
     }
 
 
+
 def handler(event, context):
 
     print("Received event: " + json.dumps(event, indent=2))
-    payload = json.loads(event['body'])
-    payload["SourceLambda"] = "GET"
-    return respond(None, payload)
+    response = dynamo.get_item(TableName='EarthdailyAtmStack-AtmDB-ZY0LE9NKVG3H',
+                           Key={
+                               'id': {
+                                   'S': '1'
+                                }
+                           })
+
+    return respond(None, response)
 
 
 
