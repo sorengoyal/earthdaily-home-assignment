01209fa8d039f918944efe49add7b1dda38d4df9
Soren Goyal <sorengoyal@gmail.com>
2023-04-24 10:46:39 -0700

    Updated lambda code to read from DynamoDB (#1)
    Added a new role to lambda to read from dynamodb
        Ref: https://github.com/widdix/aws-cf-templates/blob/907515a62195730894d0c40f517c1b844f5e4f42/security/config.yaml#L184
    
        Added calls to dyanmodb
        Ref: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/client/put_item.html
    
    Co-authored-by: Soren Goyal <sorengoyal@Sorens-MBP.hitronhub.home>

--- function/post.py
+++ function/post.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import base64
 
 print('Loading function')
 dynamo = boto3.client('dynamodb')
@@ -18,9 +19,24 @@ def respond(err, res=None):
 def handler(event, context):
 
     print("Received event: " + json.dumps(event, indent=2))
-    payload = json.loads(event['body'])
-    payload["SourceLambda"] = "POST"
-    return respond(None, payload)
+    payload = json.loads(base64.b64decode(event['body']).decode('utf-8'))
+    response = dynamo.put_item(TableName='EarthdailyAtmStack-AtmDB-ZY0LE9NKVG3H',
+                    Item={
+                        'id': {
+                            'S': payload['id'],
+                        },
+                        'address': {
+                            'S': payload['address'],
+                        },
+                        'provider': {
+                            'S': payload['provider'],
+                        },
+                        'rating': {
+                            'N': str(payload['rating']),
+                        }
+                    }
+                    )
+    return respond(None, response)
 
 
 
